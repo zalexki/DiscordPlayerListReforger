1d3a48b304289824db8c0f7ad6e1c867a87f8cea
zalexki <a.haumaitre@cityscoot.eu>
2023-05-21 04:42:22 +0200

    fix-time

--- Services/DiscordClient.cs
+++ Services/DiscordClient.cs
@@ -70,7 +70,7 @@ public class DiscordClient
                 .AddField("Time", data.ServerInfo.TimeInGame, true)
                 .AddField("Wind", RabbitToDiscordConverter.GetWindData(data.ServerInfo), true)
 
-                .WithFooter(footer => footer.Text = $"Updated at {DateTime.UtcNow:M/d/yy HH:mm:ss} UTC")
+                .WithFooter(footer => footer.Text = $"Updated at {DateTime.UtcNow:dd/MM/yy HH:mm:ss} UTC")
                 .WithColor(Color.DarkTeal);
                 // .WithCurrentTimestamp();
             
