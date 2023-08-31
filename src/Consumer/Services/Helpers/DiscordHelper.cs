c59425441f77ac73e04d3ecf52fcf331a01323d4
zalexki <a.haumaitre@cityscoot.eu>
2023-08-31 18:29:31 +0200

    trim too long text

--- src/Consumer/Services/Helpers/DiscordHelper.cs
+++ src/Consumer/Services/Helpers/DiscordHelper.cs
@@ -116,16 +116,16 @@ public class DiscordHelper
                 .AddField("** **", "** **")
                 
                 .AddField("▬▬▬▬▬▬▬▬▬▬ Server Information ▬▬▬▬▬▬▬▬▬▬", "╰┈➤")
-                .AddField("Active players", players, true)
-                .AddField("Server", server, true)
+                .AddField("Active players", players.Substring(0, 1024), true)
+                .AddField("Server", server.Substring(0, 1024), true)
                 
                 // empty line
                 .AddField("** **", "** **")
                 
                 .AddField("▬▬▬▬▬▬▬▬▬▬ Mission Information ▬▬▬▬▬▬▬▬▬▬", "╰┈➤")
-                .AddField("Mission", missionName, true)
-                .AddField("Time", data.ServerInfo.TimeInGame, true)
-                .AddField("Wind", wind, true)
+                .AddField("Mission", missionName.Substring(0, 1024), true)
+                .AddField("Time", data.ServerInfo.TimeInGame.Substring(0, 1024), true)
+                .AddField("Wind", wind.Substring(0, 1024), true)
                 
                 // empty line
                 .AddField("** **", "** **")
