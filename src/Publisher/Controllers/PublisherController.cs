a7b689dea3296ba85e5ddc1d38e5913659d6ecdf
zalexki <a.haumaitre@cityscoot.eu>
2023-08-31 19:16:22 +0200

    try fix encoding issues

--- src/Publisher/Controllers/PublisherController.cs
+++ src/Publisher/Controllers/PublisherController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -28,7 +29,7 @@ public class PublisherController : ControllerBase
     public IActionResult Healthcheck()
     {
         _logger.LogInformation("healthcheck ok");
-        
+
         return Ok("ok");
     }
     
@@ -64,7 +65,7 @@ public class PublisherController : ControllerBase
         ServerGameData gameData;
         try
         {
-            gameData = JsonConvert.DeserializeObject<ServerGameData>(content);
+            gameData = JsonConvert.DeserializeObject<ServerGameData>(Utf8ToUtf16(content));
         }
         catch (Exception e)
         {
@@ -80,11 +81,58 @@ public class PublisherController : ControllerBase
         _rabbit.Channel.BasicPublish(exchange: string.Empty,
             routingKey: ServerGameData.QueueName,
             basicProperties: null,
-            body: Encoding.Unicode.GetBytes(JsonConvert.SerializeObject(gameData)),
+            body: Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(gameData)),
             mandatory: true);
         
         _logger.LogInformation("success sent json to rabbit");
     
         return Ok();
+    }
+    
+    public static string Utf8ToUtf16(string utf8String)
+    {
+        /***************************************************************
+         * Every .NET string will store text with the UTF-16 encoding, *
+         * known as Encoding.Unicode. Other encodings may exist as     *
+         * Byte-Array or incorrectly stored with the UTF-16 encoding.  *
+         *                                                             *
+         * UTF-8 = 1 bytes per char                                    *
+         *    ["100" for the ansi 'd']                                 *
+         *    ["206" and "186" for the russian '?']                    *
+         *                                                             *
+         * UTF-16 = 2 bytes per char                                   *
+         *    ["100, 0" for the ansi 'd']                              *
+         *    ["186, 3" for the russian '?']                           *
+         *                                                             *
+         * UTF-8 inside UTF-16                                         *
+         *    ["100, 0" for the ansi 'd']                              *
+         *    ["206, 0" and "186, 0" for the russian '?']              *
+         *                                                             *
+         * First we need to get the UTF-8 Byte-Array and remove all    *
+         * 0 byte (binary 0) while doing so.                           *
+         *                                                             *
+         * Binary 0 means end of string on UTF-8 encoding while on     *
+         * UTF-16 one binary 0 does not end the string. Only if there  *
+         * are 2 binary 0, than the UTF-16 encoding will end the       *
+         * string. Because of .NET we don't have to handle this.       *
+         *                                                             *
+         * After removing binary 0 and receiving the Byte-Array, we    *
+         * can use the UTF-8 encoding to string method now to get a    *
+         * UTF-16 string.                                              *
+         *                                                             *
+         ***************************************************************/
+
+        // Get UTF-8 bytes and remove binary 0 bytes (filler)
+        List<byte> utf8Bytes = new List<byte>(utf8String.Length);
+        foreach (byte utf8Byte in utf8String)
+        {
+            // Remove binary 0 bytes (filler)
+            if (utf8Byte > 0) {
+                utf8Bytes.Add(utf8Byte);
+            }
+        }
+
+        // Convert UTF-8 bytes to UTF-16 string
+        return Encoding.UTF8.GetString(utf8Bytes.ToArray());
     }
 }
