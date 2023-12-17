9a3bebaee17dd60175e9ad11411544e68ef8add2
zalexki <a.haumaitre@cityscoot.eu>
2023-12-17 14:19:59 +0100

    fix

--- src/Consumer/Services/Helpers/TableToImageHelper.cs
+++ src/Consumer/Services/Helpers/TableToImageHelper.cs
@@ -6,12 +6,12 @@ namespace DiscordPlayerListConsumer.Services.Helpers;
 
 public static class TableToImageHelper
 {
-    public static Image GenerateTable(List<PlayerInfo> players)
-    {
-        var generator = new TableGenerator()
-        generator.LoadFromObjects<PlayerInfo>(players, x => x.Kills);
-        var bitmap = generator.ExportTableToImage(); 
+    // public static Image GenerateTable(List<PlayerInfo> players)
+    // {
+    //     var generator = new TableGenerator();
+    //     generator.LoadFromObjects<PlayerInfo>(players, x => x.Kills);
+    //     var bitmap = generator.ExportTableToImage(); 
 
-        return bitmap;
-    }
+    //     return bitmap;
+    // }
 }
